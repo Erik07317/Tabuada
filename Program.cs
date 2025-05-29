b03fbd6bcef593a8dc9e9dc98ad672ca4a4405bd
Erik Sabino <eriksabino@Eriks-MacBook-Air.local>
2025-05-29 16:07:06 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Escreva um número para ser exibida a tabuada: ");
+int numero = int.Parse(Console.ReadLine());
+Console.WriteLine($"Tabuada do {numero}:");
+for (int i = 1; i <= 10; i++)
+{
+    int resultado = numero * i;
+    Console.WriteLine($"{numero} x {i} = {resultado}");
+}
+Console.WriteLine("Tabuada concluída.");
